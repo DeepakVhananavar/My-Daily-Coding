e4b661cb3d9574baf425ebeab1881c5f19f0e974
Deepak <71967133+DeepakVhananavar@users.noreply.github.com>
2022-06-24 11:35:26 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,40 +1,65 @@
-// Accept anumber and display its fibnoacci series
+// Accept N numbers from user.
+//Accept one another number and return the nearest  umber to that number.
 
+// Input : N = 10
+// Element = 15
+//	5	8	12	4	-8	8	10	2	2	0
+// Output :	12	
 
+// Input : N = 10
+// Element = 2
+//	5	8	12	4	-8	8	10	2	2	0
+// Output :	2	
+
+// Input : N = 10
+// Element = 7
+//	5	8	12	4	-8	8	10	2	2	0
+// Output :	8
+
+// Input : N = 10
+// Element = 25
+//	5	8	12	4	-8	8	10	2	2	0
+// Output :	12	
 
 import java.lang.*;
 import java.util.*;
-
-class Numbers
+class DemoArray
 {
-	
-
-	void Fibbonacci(int iNo)
+	int Nearest(int arr[], int iNo)
 	{
-		int first=0,Second=1,third=0;
-		System.out.println();
-		for(int i=1;i<=iNo;i++)
+		int near=0,i=0;
+		near=arr[i];
+		for(i=1;i<arr.length;i++)
 		{
-			System.out.println(first+"\t");
-			third=first + Second;
-			first=Second;
-			Second=third;
+			if((arr[i]>near) && (arr[i]<=iNo))
+			{
+				near=arr[i];
+			}
+
 		}
+		return near;
+
 	}
-
 }
-
-
-
 class Demo
 {
-	public static void main(String args[])
+	public static void main(String arg[])
 	{
-		
-		Scanner sobj=new Scanner(System.in);
-		System.out.println("Enter the Number:");
-		int value=sobj.nextInt();
-		Numbers aobj=new Numbers();
-		aobj.Fibbonacci(value);
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter number of elements");
+		int size = sobj.nextInt();
+		int arr[] = new int[size];
+		System.out.println("Enter elements");
+		for(int i = 0; i < arr.length; i++)
+		{
+			arr[i] = sobj.nextInt();
 		}
+		System.out.println("Enter the number");
+		int value = sobj.nextInt();
+		DemoArray dobj = new DemoArray();
+		int ret = dobj.Nearest(arr,value);
+
+		System.out.println("Nearest number is "+ret);
+	}
 }
+
